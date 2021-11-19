68e6b93920ce37a3cb0c1d08d7a802e18ad97fea
Yevhenii <freshjs.fte@gmail.com>
2021-11-19 21:03:26 +0200

    Add user crud

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const userRouter = require("./routes/users");
+
+/* connection */
 const mongoConfig = require("./configs/mongodb");
-
-const app = express();
-
 const mode = process.env.NODE_ENV || "development";
 const dbConfig = mongoConfig[mode];
-
 mongoose.connect(
   `mongodb://${dbConfig.hostname}:${dbConfig.port}/${dbConfig.dbName}`
 );
 
-app.use("/", (req, res) => {
+/* app */
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
   res.status(200).send("Hello, world!");
 });
+
+app.use("/api/users", userRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,51 @@
-const userRouter = require('express').Router();
+const userRouter = require("express").Router();
+const User = require("../models/user");
 
-userRouter.get('/', (req, res, next) => {
-    try {
-        res.send('all users')
-    } catch (error) {
-        next(error);
+userRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.post("/", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const createdUser = await User.create(req.body);
+    res.send(createdUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.put("/:userId", async (req, res, next) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      { ...req.body },
+      { returnDocument: "after" }
+    );
+    res.send(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.delete("/:userId", async (req, res, next) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.userId);
+    if (!deletedUser) {
+      return res.status(404).send({ error: "User not found" });
     }
-})
 
-userRouter.post('/', (req, res, next) => {
-    try {
-        res.send('created users')
-    } catch (error) {
-        next(error);
-    }
-})
+    res
+      .status(200)
+      .send({ data: deletedUser, meta: { userId: req.params.userId } });
+  } catch (error) {
+    next(error);
+  }
+});
 
-userRouter.put('/', (req, res, next) => {
-    try {
-        res.send('updated users')
-    } catch (error) {
-        next(error);
-    }
-})
-
-
-userRouter.delete('/', (req, res, next) => {
-    try {
-        res.send('deleted users')
-    } catch (error) {
-        next(error);
-    }
-})
-
+module.exports = userRouter;
