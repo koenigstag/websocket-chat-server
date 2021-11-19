827ddc96e4c17a69d7974c2978e7210ed9d1040d
Yevhenii <freshjs.fte@gmail.com>
2021-11-19 19:32:58 +0200

    Add configs

---
+++ configs/mongodb.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    hostname: "localhost",
+    port: 27017,
+    dbName: "fe_mongoose",
+  },
+  test: {},
+  production: {},
+};
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const app = require('./app');
+const config = require('./configs');
 
-const PORT = 5000;
+const PORT = config.PORT || 5000;
 http.createServer(app).listen(PORT, () => {
     console.log("App is listening on PORT", PORT);
 })
