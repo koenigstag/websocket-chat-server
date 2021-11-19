1c4c5dacf371bd742fe2fa0625daeeb137d97bd0
Yevhenii <freshjs.fte@gmail.com>
2021-11-19 19:22:13 +0200

    Add basic server structure

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const app = require('./app');
+
+const PORT = 5000;
+http.createServer(app).listen(PORT, () => {
+    console.log("App is listening on PORT", PORT);
+})
