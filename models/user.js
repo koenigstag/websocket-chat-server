1ade03fdeef74417dcce7aa8d2bff1eadd7ec10a
Yevhenii <freshjs.fte@gmail.com>
2021-11-22 18:23:34 +0200

    Refactor user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,37 @@
-const { model } = require("mongoose");
-const user = require("../schemes/user");
+const { model, Schema } = require("mongoose");
 
-module.exports = model("users", user);
+const userShema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "Moder", "Member"],
+      required: true,
+    },
+    isMale: {
+      type: Boolean,
+      required: true,
+    },
+    birthday: {
+      type: Date,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = model("users", userShema);
