9362896ceaeebbffdec6858e1ec27c76951194bf
Yevhenii <freshjs.fte@gmail.com>
2021-11-22 19:02:13 +0200

    Install cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const rootRouter = require("./routes");
 
 /* app */
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello, world!");
