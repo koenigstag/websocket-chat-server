82c074172a9d23c237c2bde39cf02866e8b6b4de
Yevhenii <freshjs.fte@gmail.com>
2021-11-22 18:23:19 +0200

    Add user controller

---
+++ controllers/user.js
@@ -0,0 +1,47 @@
+const { user: User } = require("../models");
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.createUser = async (req, res, next) => {
+  try {
+    const createdUser = await User.create(req.body);
+    res.send(createdUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateUser = async (req, res, next) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      { ...req.body },
+      { returnDocument: "after" }
+    );
+    res.send(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteUser = async (req, res, next) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.userId);
+    if (!deletedUser) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    res
+      .status(200)
+      .send({ data: deletedUser, meta: { userId: req.params.userId } });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,12 @@
 const userRouter = require("express").Router();
-const User = require("../models/user");
+const UserController = require("../controllers/user");
 
-userRouter.get("/", async (req, res, next) => {
-  try {
-    const users = await User.find();
-    res.send(users);
-  } catch (error) {
-    next(error);
-  }
-});
+userRouter.get("/", UserController.getAllUsers);
 
-userRouter.post("/", async (req, res, next) => {
-  try {
-    console.log(req.body);
-    const createdUser = await User.create(req.body);
-    res.send(createdUser);
-  } catch (error) {
-    next(error);
-  }
-});
+userRouter.post("/", UserController.createUser);
 
-userRouter.put("/:userId", async (req, res, next) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.userId,
-      { ...req.body },
-      { returnDocument: "after" }
-    );
-    res.send(updatedUser);
-  } catch (error) {
-    next(error);
-  }
-});
+userRouter.put("/:userId", UserController.updateUser);
 
-userRouter.delete("/:userId", async (req, res, next) => {
-  try {
-    const deletedUser = await User.findByIdAndDelete(req.params.userId);
-    if (!deletedUser) {
-      return res.status(404).send({ error: "User not found" });
-    }
-
-    res
-      .status(200)
-      .send({ data: deletedUser, meta: { userId: req.params.userId } });
-  } catch (error) {
-    next(error);
-  }
-});
+userRouter.delete("/:userId", UserController.deleteUser);
 
 module.exports = userRouter;
