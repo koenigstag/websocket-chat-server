9fb371f1e6ded254a33b19b0c9a4ce95147854ca
Yevhenii <freshjs.fte@gmail.com>
2021-11-22 18:53:23 +0200

    Add msg controller

---
+++ controllers/message.js
@@ -0,0 +1,35 @@
+const { message: Message } = require("../models");
+
+module.exports.getAllMsgs = async (req, res, next) => {
+  try {
+    const filter = {};
+    if (req.params.userId) {
+      filter.author = req.params.userId;
+    }
+
+    const msgs = await Message.find(filter)
+      .limit(10)
+      .sort({ createdAt: 1 })
+      .populate("author");
+
+    res.send(msgs);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.createMsg = async (req, res, next) => {
+  try {
+    // req.body.author = req.params.userId;
+
+    console.log("create ", req.params.userId);
+
+    const createdMsg = await Message.create({
+      ...req.body,
+      author: req.params.userId,
+    });
+    res.send(createdMsg);
+  } catch (error) {
+    next(error);
+  }
+};
